45829622f6007e47f8cea440e96c7558bae7f091
alvaroolivaresflores <alvaro.olivares.flores@hotmail.com>
2021-06-06 14:48:44 -0400

    v2.8

--- src/app/models/entrepreneur.ts
+++ src/app/models/entrepreneur.ts
@@ -1,15 +1,45 @@
 import { Service } from './service';
-import { User } from './user'
+import { User } from './user';
 
 export class Entrepreneur extends User {
-    private services: Array<Service>;
-    private profilePicture: string;
-    private description: string;
+  private _services: Array<Number>;
+  private profilePicture: string;
+  private description: string;
 
-    public constructor(id: number, name: string, lastname: string, birthDay: Date, eMail: string, gender: string, region: string, contratedServices: Array<Service>, serices: Array<Service>, profilePicture: string, description: string) {
-        super(id,name,lastname,birthDay,eMail,gender,region,contratedServices);
-        this.services = serices;
-        this.profilePicture = profilePicture;
-        this.description = description;
-    }
+  public constructor(
+    id: number,
+    name: string,
+    lastname: string,
+    birthDay: string,
+    eMail: string,
+    gender: string,
+    region: string,
+    contratedServices: Array<Service>,
+    services: Array<Number>,
+    profilePicture: string,
+    description: string
+  ) {
+    super(
+      id,
+      name,
+      lastname,
+      birthDay,
+      eMail,
+      gender,
+      region,
+      contratedServices
+    );
+    this._services = services;
+    this.profilePicture = profilePicture;
+    this.description = description;
+  }
+  get services() {
+    return this._services;
+  }
+  get _profilePicture() {
+    return this.profilePicture;
+  }
+  get _description() {
+    return this.description;
+  }
 }
