d92460da18902d98d0cfcaee3904c3e8a533616a
JoaquinVidal20 <juaco.vidal.martinez@gmail.com>
2021-06-10 18:48:22 -0400

    juacov 0.1

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Service } from 'src/app/models/service';
+import { ServicesService } from 'src/app/services/services.service';
 
 export interface HiredServicesData {
   name: string;
@@ -10,18 +12,6 @@ export interface HiredServicesData {
   state: string;
 }
 
-const ELEMENT_DATA: HiredServicesData[] = [
-  { position: 1, name: 'Servicio 1', price: 179, state: 'Finalizado' },
-  { position: 2, name: 'Servicio 2', price: 402, state: 'Finalizado' },
-  { position: 3, name: 'Servicio 3', price: 641, state: 'En curso' },
-  { position: 4, name: 'Servicio 4', price: 922, state: 'Finalizado' },
-  { position: 5, name: 'Servicio 5', price: 101, state: 'Finalizado' },
-  { position: 6, name: 'Servicio 6', price: 107, state: 'Finalizado' },
-  { position: 7, name: 'Servicio 7', price: 167, state: 'Finalizado' },
-  { position: 8, name: 'Servicio 8', price: 194, state: 'Finalizado' },
-  { position: 9, name: 'Servicio 9', price: 184, state: 'Finalizado' },
-  { position: 10, name: 'Servicio 10', price: 297, state: 'Finalizado' },
-];
 
 @Component({
   selector: 'app-table',
@@ -29,10 +19,22 @@ const ELEMENT_DATA: HiredServicesData[] = [
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements AfterViewInit {
-  displayedColumns: string[] = ['position', 'name', 'state', 'price'];
+  Element_hired: Service[];
+  dataSource: MatTableDataSource<Service>;
+  displayedColumns: string[] = ['position', 'name', 'price'];
 
-  ngOnInit(): void {}
-  dataSource = new MatTableDataSource<HiredServicesData>(ELEMENT_DATA);
+  ngOnInit(): void {
+    this.loadServices();
+  }
+
+  async loadServices() {
+    this.Element_hired=await this.hired.getServices().toPromise();
+    this.dataSource=new MatTableDataSource<Service>(this.Element_hired);
+    this.dataSource.paginator=this.paginator;
+    this.dataSource.sort=this.sort;
+  }
+
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -42,5 +44,8 @@ export class TableComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-  constructor() {}
+  constructor(private hired:ServicesService,) {
+    this.Element_hired=[];
+    this.dataSource=new MatTableDataSource<Service>();
+  }
 }
